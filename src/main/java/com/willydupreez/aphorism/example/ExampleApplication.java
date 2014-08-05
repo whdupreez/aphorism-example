1b6cd23058211395cb070030faa790c163db46c1
whdupreez <willydupreez@gmail.com>
2014-08-05 22:29:54 +0200

    Basic application structure.

--- src/main/java/com/willydupreez/aphorism/example/ExampleApplication.java
+++ src/main/java/com/willydupreez/aphorism/example/ExampleApplication.java
@@ -1,5 +1,18 @@
 package com.willydupreez.aphorism.example;
 
-public class ExampleApplication {
+import com.willydupreez.aphorism.app.Application;
+import com.willydupreez.aphorism.daemon.ApplicationEnvironment;
+
+public class ExampleApplication implements Application {
+
+	@Override
+	public void onStart(ApplicationEnvironment context) {
+
+	}
+
+	@Override
+	public void onStop() {
+
+	}
 
 }
---
+++ src/test/java/com/willydupreez/aphorism/example/ExampleApplicationTest.java
@@ -0,0 +1,11 @@
+package com.willydupreez.aphorism.example;
+
+import com.willydupreez.aphorism.daemon.Launcher;
+
+public class ExampleApplicationTest {
+
+	public static void main(String[] args) throws Exception {
+		Launcher.main(new String[] { "run" });
+	}
+
+}
